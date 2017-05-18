8d504681982678b7d1c22e5bbef7d8c1eabfd99f
xuyanping <xuyanping@xiaoyouzi.com>
2017-05-18 14:16:47 +0800

    任务优先级，买二赠一最优

--- apps/services/PolicyFactory.php
+++ apps/services/PolicyFactory.php
@@ -10,12 +10,13 @@ class PolicyFactory{
 
     public static function create( $serial_number ){
         switch ( $serial_number ) {
-            case 'MEI0001':
-                return new DonatePolicy(2);
-                break;
 
             case 'MEI0002':
                 return new DiscountPolicy(95);
+                break;
+
+            case 'MEI0001':
+                return new DonatePolicy(2);
                 break;
 
             default:
